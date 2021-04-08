a3ace27dc49abe03317e1daf790f0446231d2488
Pricemou <scameleons3@gmail.com>
2021-04-08 23:12:47 +0000

    episode2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class List extends Component{
+  render(){
+    return(
+      <div className="liste"> ici c'est ma liste </div>
+    );
+  }
 }
 
-export default App;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Apprendre ReactJS
+          </h2>
+        </div>
+        <p className="App-intro">
+          <List/>
+        </p>
+      </div>
+    )
+  } 
+}
+
+export default App
